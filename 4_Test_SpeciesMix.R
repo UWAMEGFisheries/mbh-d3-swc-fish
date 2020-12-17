55c32531f66b3422ee02737a57c2074de66a3d0d
anitas.giraldo <anita.giraldo@marineecology.io>
2020-12-17 16:05:23 +0800

    commit

---
+++ 4_Test_SpeciesMix.R
@@ -0,0 +1,11 @@
+### Testing SpeciesMix script ####
+# Fit Mixtures of Archetype Species # 
+
+
+# I have not been successfull at installing this package: SpeciesMix
+# It has been retired from CRAN 
+
+#install.packages("//uniwa.uwa.edu.au/userhome/staff1/00093391/My Documents/R/SpeciesMix-master.zip", type="binary", repos=NULL)
+install.packages("//uniwa.uwa.edu.au/userhome/staff1/00093391/My Documents/R/SpeciesMix.zip", type="binary", repos=NULL)
+
+library(SpeciesMix)
