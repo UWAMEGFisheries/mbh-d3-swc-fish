8e3dfc5747d33a4f1d8af5fa35c41d6dfdcdaed4
anitas.giraldo <anita.giraldo@marineecology.io>
2021-06-10 08:29:44 +0800

    commit

--- econarrative/Sea_level_map.R
+++ econarrative/Sea_level_map.R
@@ -124,17 +124,8 @@ ybf11k <- bathy1
 ybf11k[ybf11k < (-40)] <- NA
 plot(ybf11k, col="red")
 plot(wa1, add=T)
-e <- drawExtent()
-e
-e2 <- drawExtent()
 ybf11k[ybf11k > (-40)] <- 1
 
-
-x <- raster()
-
-test <- bathy1
-test[test < (-39)] <- NA
-plot(test)
 
 plot(ybf21k, col="blue")
 plot(ybf70k, col="green", add=T)
@@ -158,22 +149,22 @@ plot(k11)
 
 
 ## TRYING SOME STUFF ----
-k11$OBJECTID <- '1'
-wa2 <- wa1
-wa2$OBJECTID <- '11'
-wa2 <- wa2[,-c(2:7)]
-plot(wa2)
-k11 <- k11[,-1]
-k11a <- raster::merge(k11, wa1)
-plot(k11a)
-k11a$OBJECTID
-k <- k11a[k11a$OBJECTID.1 == 1,]
-plot(k)
-w <- k11a[k11a$OBJECTID.2 == 11,]
-plot(w)
-k11b <- raster::aggregate(rbind(wa2,k11))
-plot(k11b, col='red')
-k11b$OBJECTID
+# k11$OBJECTID <- '1'
+# wa2 <- wa1
+# wa2$OBJECTID <- '11'
+# wa2 <- wa2[,-c(2:7)]
+# plot(wa2)
+# k11 <- k11[,-1]
+# k11a <- raster::merge(k11, wa1)
+# plot(k11a)
+# k11a$OBJECTID
+# k <- k11a[k11a$OBJECTID.1 == 1,]
+# plot(k)
+# w <- k11a[k11a$OBJECTID.2 == 11,]
+# plot(w)
+# k11b <- raster::aggregate(rbind(wa2,k11))
+# plot(k11b, col='red')
+# k11b$OBJECTID
 
 
 
@@ -185,7 +176,7 @@ df <- df[-c(2,3),]
 df
 dfs <- df
 coordinates(dfs) <- ~Lon+Lat
-plot(dfs)
+#plot(dfs)
 
 
 ### Map ----
@@ -341,31 +332,33 @@ plot(wa1sm2) # THIS ISN'T WORKING
 
 
 map <- tm_shape(cmr1, bbox=ext1)  + tm_borders(col ='white', lwd = 1.5) +
-  tm_compass(type = "arrow", position = c(0.1, 0.1), size = 2) +
+  tm_compass(type = "arrow", position = c(0.23, 0.1), size = 2) +
   #tm_fill(col ='ZONENAME', palette=c('yellow', 'red'), alpha = 0.1) +
-  tm_scale_bar(breaks = c(0, 10, 20), text.size = 0.5, position = c(0.08, 0.02))  
+  tm_scale_bar(breaks = c(0, 10, 20), text.size = 0.5, position = c(0.2, 0.02))  
   #tm_graticules(ticks = FALSE) +
   #tm_grid(n.x = 3, n.y = 3, labels.size = 1.5, lines = FALSE) 
 map
 
 map0 <- map + tm_shape(bathy1) + tm_raster(title = 'Depth (m)', palette=get_brewer_pal("Blues", plot=FALSE), style='cont', legend.reverse = T) +
-  tm_layout(#outer.margins = 0.1,
+  #tm_add_legend(type = 'fill', labels = "xxx", col = g2, lwd = 2, size = 2) +
+ tm_layout(outer.margins = 0.01,
+           inner.margins = 0,
             legend.text.size = 1,
-            legend.outside = TRUE,
+            #legend.outside = FALSE,
             #legend.outside.position = 'right',
-            legend.outside.position = 'right',
+            #legend.outside.position = 'right',
             #legend.stack = 'horizontal',
             #legend.is.protrait = TRUE,
-            #legend.position = c(1, 0.05),
+            legend.position = c(0.01, 0.72),
             legend.title.size = 1.5,
             legend.title.color = 'black',
-            legend.width = 1) 
+            legend.width = 1)
 map0
 
 
 
-map1 <- map0 + tm_shape(k21sm) + tm_borders(col = g2, lwd = 2) + tm_fill(col=g2) +
-  tm_add_legend(type = 'fill', labels = "21,000 years ago", col = g2, lwd = 2, size = 2)
+map1 <- map0 + tm_shape(k21sm) + tm_borders(col = g2, lwd = 2) + tm_fill(col=g2) 
+  #tm_add_legend(type = 'fill', labels = "21,000 years ago", col = g2, lwd = 2, size = 2)
 
 map1
 
@@ -375,12 +368,12 @@ map1
 #map2
 
 
-map3 <- map1 + tm_shape(k11sm) + tm_borders(col = g4, lwd = 2) + tm_fill(g4) +
-  tm_add_legend(type = 'fill', labels = "11,000 years ago", col = g4, lwd = 2, size = 2)
+map3 <- map1 + tm_shape(k11sm) + tm_borders(col = g4, lwd = 2) + tm_fill(g4) 
+ # tm_add_legend(type = 'fill', labels = "11,000 years ago", col = g4, lwd = 2, size = 2)
 map3
 
-map4 <- map3 + tm_shape(wa1sm) + tm_borders(col = 'grey70', lwd = 2) + tm_fill(col=sg) +
-  tm_add_legend(type = 'fill', labels = "Coastline today", col = sg, lwd = 2, size = 2)
+map4 <- map3 + tm_shape(wa1sm) + tm_borders(col = 'grey70', lwd = 2) + tm_fill(col=sg) 
+  #tm_add_legend(type = 'fill', labels = "Coastline today", col = sg, lwd = 2, size = 2)
 map4
 
 #map5 <- map4 + tm_shape(dfs) + tm_symbols(col = 'black', size = 0.2) +
@@ -396,8 +389,9 @@ map55
 #tm_add_legend(type = 'line', labels = "Special Purpose Zone", col = 'black', lwd = 2, size = 2)
 #map6
 
-map7 <- map55 + tm_shape(NPZ) + tm_borders(col = "#006600", lwd = 1.5) + 
-  tm_add_legend(type = 'fill', labels = "National Park Zone and State Sanctuary Zones", col = 'white', border.col= "#006600", border.lwd = 3, size = 2)
+map7 <- map55 + tm_shape(NPZ) + tm_borders(col = "#006600", lwd = 1.5) 
+  #tm_add_legend(type = 'fill', labels = "National Park Zone and State Sanctuary Zones", col = 'white', border.col= "#006600", border.lwd = 3, size = 2)
+                
 map7
 
 map71 <- map7 + tm_shape(dfs) + tm_symbols(col = 'black', size = 0.2) +
@@ -408,13 +402,47 @@ map8 <- map71 + tm_shape(wamp1) + tm_borders(col = "#006600", lwd = 1.5)  #+ tm_
   #tm_add_legend(type = 'fill', col = 'white', border.col = 'white', labels = 'State Sanctuary Zones')
 map8
 
-map9 <- map8 + tm_shape(cw1) + tm_lines(col = 'red', lwd = 1) +
-  tm_add_legend(type = 'fill', col = 'white', border.col = 'red', border.lwd =3, labels = 'Commonwealth/State waters boundary')
+map9 <- map8 + tm_shape(cw1) + tm_lines(col = 'red', lwd = 1) 
+  # #tm_add_legend(type = 'fill', col = 'white', border.col = 'red', border.lwd =3, labels = 'Commonwealth/State waters boundary') +
+  # tm_layout(#outer.margins = 0.1,
+  #   legend.text.size = 1,
+  #   #legend.outside = TRUE,
+  #   #legend.outside.position = 'right',
+  #   #legend.outside.position = 'right',
+  #   #legend.only = TRUE,
+  #   legend.title.size = 1.5,
+  #   legend.title.color = 'black',
+  #   legend.width = 1) +
+  # tm_add_legend(type = 'fill', labels = "21,000 years ago (-125 metres)", col = g2, lwd = 2, size = 2) + 
+  # tm_add_legend(type = 'fill', labels = "11,000 years ago (-40 metres)", col = g4, lwd = 2, size = 2) +
+  # tm_add_legend(type = 'fill', labels = "Coastline today", col = sg, lwd = 2, size = 2) +
+  # tm_add_legend(type = 'fill', col = 'white', border.col = 'red', border.lwd =3, labels = 'Commonwealth/State waters boundary', size = 3) +
+  # tm_add_legend(type = 'fill', labels = "National Park Zone and State Sanctuary Zones", col = 'white', border.col= "#006600", border.lwd = 3, size = 2)
+ 
 map9
 
+
+map.leg <- tm_shape(cmr1) + tm_borders(col ='white', lwd = 1.5) +
+  #tm_shape(cw1) + tm_lines(col = 'red', lwd = 1) +
+   tm_layout(#outer.margins = 0.1,
+  legend.text.size = 2,
+    #legend.outside = TRUE,
+  #   #legend.outside.position = 'right',
+  #   #legend.outside.position = 'right',
+  legend.only = TRUE) +
+  #   legend.title.size = 1.5,
+  #   legend.title.color = 'black',
+   #legend.width = 1) +
+  tm_add_legend(type = 'fill', labels = "21,000 years ago (-125 metres)", col = g2, lwd = 2, size = 4) + 
+  tm_add_legend(type = 'fill', labels = "11,000 years ago (-40 metres)", col = g4, lwd = 2, size = 4) +
+  tm_add_legend(type = 'fill', labels = "Coastline today", col = sg, lwd = 2, size = 4) +
+  tm_add_legend(type = 'fill', col = 'white', border.col = 'red', border.lwd =3, labels = 'Commonwealth/State waters boundary', size = 4) +
+  tm_add_legend(type = 'fill', labels = "National Park Zone and State Sanctuary Zones", col = 'white', border.col= "#006600", border.lwd = 3, size = 4)
+ 
+
+map.leg
+
 ####
-
-
 
 
 # save ----
@@ -426,3 +454,39 @@ tmap_save(
   #units = NA,
   #scale = 1,
   dpi = 300)
+
+
+
+#### To create the inset map ----
+
+### 1. define area of interest: part of WA ####
+
+sw_region <- st_bbox(ext1,
+                     crs = st_crs(bathy1)) %>%
+  st_as_sfc()
+
+### 1.1 define area of interest: Rottnest Island ####
+extent(ri)
+
+
+
+ri_region <- st_bbox(c(xmin = 115.4242, xmax = 115.5675,
+                       ymin = -32.06073 , ymax = -31.97739),
+                     crs = st_crs(ri)) %>%
+  st_as_sfc()
+
+### 2.1 create a base map: Austalia ###
+
+# read Australia shapefile ----
+a <- readOGR(paste(s.dir, "STE11aAust.shp", sep='/'))
+plot(a)
+asm <- st_as_sf(a)
+asm <- drop_crumbs(asm, set_units(10, km^2))
+plot(asm)
+asm2 <- fill_holes(asm, set_units(20, km^2))
+asm3 <- smoothr::smooth(asm2, method = "ksmooth", smoothness = 20)
+plot(asm3)
+
+a_map <- tm_shape(wa, bbox = wa_region) + tm_polygons(border.col = "black")
+wa2_map
+
